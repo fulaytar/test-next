a33380a62eacf987be97ef93cdc099dccae29b72
fulaytar <fulaytar2405@gmail.com>
2024-06-19 14:30:11 +0300

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className=''>Next JS 13 Startet Kit</h1>
+      </div>
+    </section>
+  )
+}
