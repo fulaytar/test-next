688e878027defd615ebf70c830bb51ff9e62c072
fulaytar <fulaytar2405@gmail.com>
2024-06-20 16:24:04 +0300

    add slug

---
+++ app/posts/page.jsx
@@ -0,0 +1,16 @@
+const Page = () => {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <header className='rounded bg-gray-100 p-8'>
+          <h1 className='font-serif text-3xl'>Learn NextJs</h1>
+          <p className='text-sm font-light uppercase leading-3'>
+            Ivan Stepanowich
+          </p>
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
